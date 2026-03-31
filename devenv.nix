8d32562d71dbc07a5c9a1600638c6f2e176bb2cc
Joakim Ohlrogge <joakim.ohlrogge@agical.se>
2026-03-31 12:21:31 +0200

    chore: add git pre-commit hook for clippy via devenv git-hooks

--- devenv.nix
+++ devenv.nix
@@ -6,6 +6,15 @@ in
 {
   claude.code.enable = true;
 
+  git-hooks.enable = true;
+  git-hooks.hooks.clippy = {
+    enable = true;
+    name = "clippy";
+    entry = "cargo polylith cargo --profile dev clippy -- -D warnings";
+    pass_filenames = false;
+    types = ["rust"];
+  };
+
   packages = [
     pkgs.helix
   ];
